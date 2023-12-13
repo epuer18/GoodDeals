2db7445a35c0d12341f7877b4d8d0cbf1f6893e5
Chelsea <chelseacui211@gmail.com>
2023-12-12 21:57:53 -0800

    update color

--- front/src/components/AppFooter.jsx
+++ front/src/components/AppFooter.jsx
@@ -1,8 +1,8 @@
 export function AppFooter() {
   return (
-    <footer className="footer ">
+    <footer className="footer">
       <div className="container-fluid text-center">
-        <span className="text-muted">&copy; 2023 Yushi Cui & Zexi Gong</span>
+        <span>&copy; 2023 Yushi Cui & Zexi Gong</span>
       </div>
     </footer>
   );
